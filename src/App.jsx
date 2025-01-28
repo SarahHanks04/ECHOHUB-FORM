23649344095b7a265e15b987f0699b7e78869ed1
SarahHanks04 <sarahhanks04@gmail.com>
2025-01-29 00:53:49 +0100

    fix:add initial project setup with Tailwind CSS, theme toggle component, and utility functions

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import "./App.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import PageNotFound from "./Pages/PageNotFound";
+import ThemeToggle from "./Components/Themes/ThemesToggle";
 
 function App() {
   return (
     <Router>
+      <div>
+        <ThemeToggle />
+      </div>
+
       <Routes>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
---
+++ src/Components/Themes/ThemesToggle.jsx
@@ -0,0 +1,32 @@
+import { Moon, Sun } from "lucide-react";
+import { useState, useEffect } from "react";
+
+const ThemeToggle = () => {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+  useEffect(() => {
+    document.documentElement.classList.remove("light", "dark");
+    document.documentElement.classList.add(theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className="p-2 rounded-full bg-light-accent dark:bg-bulb-blue text-light-background dark:text-bulb-white shadow-md transition-all duration-300 absolute top-4 right-4"
+      aria-label="Toggle Theme"
+    >
+      {theme === "light" ? (
+        <Moon className="w-4 h-4" />
+      ) : (
+        <Sun className="w-4 h-4" />
+      )}
+    </button>
+  );
+};
+
+export default ThemeToggle;
