a257d1121be3948b5b503af4d1dc3e7a6d5561c1
SarahHanks04 <sarahhanks04@gmail.com>
2025-02-10 15:14:24 +0100

    feat: implement EventFeedbackUserView component and enhance form handling; update routing and validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes, Navigate } from "react-router-dom";
 import PageNotFound from "./pages/PageNotFound";
 import HeroPage from "./pages/HeroPage";
 import Navbar from "./components/HeroNavbar/Navbar";
@@ -14,13 +14,11 @@ function App() {
         <Navbar />
       </div>
       <Routes>
+        <Route path="/" element={<Navigate to="/feedback" />} />
         <Route path="/feedback" element={<HeroPage />} />
         {/* User Routes */}
         <Route path="form/complaint/:formId" element={<ComplaintUserView />} />
-        <Route
-          path="form/event/:formId"
-          element={<EventFeedbackUserView />}
-        />
+        <Route path="form/event/:formId" element={<EventFeedbackUserView />} />
         <Route path="*" element={<PageNotFound />} />
         {/* Admin Routes */}
         {/* <Route path="/admin/feedback/:formId" element={<AdminFeedbackView />} /> */}
